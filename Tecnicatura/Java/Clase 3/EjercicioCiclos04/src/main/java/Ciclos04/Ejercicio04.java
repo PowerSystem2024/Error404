500bf468c2efc3cc66752d1da8434636e5d683f0
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-09-16 22:26:12 -0300

    Ejercicio 4 de Ciclos en Java con la clase Scanner y JOptionPane

---
+++ Tecnicatura/Java/Clase 3/EjercicioCiclos04/src/main/java/Ciclos04/Ejercicio04.java
@@ -0,0 +1,25 @@
+/*
+Ejercicio 4: Pedir números hasta que se teclee uno negativo,
+y mostar cuántos números se han introducido.
+Lo hacemos primero con la clase Scanner
+Luego lo hacemos con la clase JOptionPane.
+ */
+package Ciclos04;
+
+import javax.swing.JOptionPane;
+
+
+public class Ejercicio04 {
+    public static void main(String[] args) {
+        int numero, conteo = 0;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        while(numero >= 0){
+           JOptionPane.showMessageDialog(null, "El número "+numero+ " es POSITIVO");
+            conteo++;
+            System.out.println("Digite otro número: ");
+           numero = Integer.parseInt(JOptionPane.showInputDialog("Digite otro número: "));
+        }
+        JOptionPane.showMessageDialog(null,"A ingresado "+conteo+" números que no son negativos");
+     
+    }
+}
