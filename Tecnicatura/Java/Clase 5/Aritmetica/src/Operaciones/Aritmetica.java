78fc79e165727b7210a9778d6ce1070f0162014c
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-09-18 19:30:27 -0300

    Clase Aritmética: Creamos un objeto

---
+++ Tecnicatura/Java/Clase 5/Aritmetica/src/Operaciones/Aritmetica.java
@@ -0,0 +1,16 @@
+
+package Operaciones;
+
+
+public class Aritmetica {
+    //Atributos de la clase
+    int a;
+    int b;
+    
+    //Metodo
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("El resultado = " + resultado);
+    }
+    
+}
