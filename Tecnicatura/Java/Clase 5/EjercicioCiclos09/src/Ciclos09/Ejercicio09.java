cc5f964dfa14e432ebe8c65432cc8ba96a6b63be
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-09-23 20:35:17 -0300

    Ejercicio Ciclos09 con Scanner y JOptionPane

---
+++ Tecnicatura/Java/Clase 5/EjercicioCiclos09/src/Ciclos09/Ejercicio09.java
@@ -0,0 +1,35 @@
+/*
+Ejercicio 9: Perdir el día, mes y año de una fecha e indicar
+si la fecha es correcta. Suponiendo que 
+todos los meses son de 30 días.
+ */
+
+package Ciclos09;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio09 {
+    public static void main(String[] args) {
+        int dia = Integer.parseInt(JOptionPane.showInputDialog("Digite el día: "));
+        int mes = Integer.parseInt(JOptionPane.showInputDialog("Digite el mes: "));
+        int anio = Integer.parseInt(JOptionPane.showInputDialog("Digite el año"));
+        if((dia != 0)&&(dia <= 30)) {
+            if((mes != 0)&&(dia <= 12)) {
+                if((anio != 0)&&(anio <= 2022)) {
+                    JOptionPane.showMessageDialog(null, "La fechha ingresada es: "+dia+"/"+mes+"/"+anio);
+                }
+                else {
+                    JOptionPane.showMessageDialog(null, "Fecha incorrecta, año incorrecto");
+                }
+            }
+            else{
+                JOptionPane.showMessageDialog(null, "Fecha incorrecta, año incorrecto");
+            }
+        }
+        else {
+        JOptionPane.showMessageDialog(null, "Fecha incorrecta, año incorrecto");    
+    }
+          
+}
+    
+}
