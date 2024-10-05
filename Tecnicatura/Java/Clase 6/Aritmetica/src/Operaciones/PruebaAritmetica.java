95f33f6f78b9ca17c66e0911e6d2c159a0c62d99
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-10-05 20:36:18 -0300

    Engineer Java: Memoria stack y heap, tratamiento de residuos

--- Tecnicatura/Java/Clase 6/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Tecnicatura/Java/Clase 6/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -4,14 +4,14 @@ package Operaciones;
 public class PruebaAritmetica {
     public static void main(String[] args) {
         var a = 10; //Variables locales
-        int b = 7;
-        miMetodo ();
+        int b = 7; //Memoria stack
+        miMetodo ();//Llamamos el método nuevo
         
         Aritmetica aritmetica1    = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
         aritmetica1.sumarNumeros();
-        
+        //Para almacenar un objeto o los atributos se utiliza la memoria heap
          int resultado = aritmetica1.sumarConRetorno();
          System.out.println("resultado = " + resultado);
          
@@ -24,6 +24,8 @@ public class PruebaAritmetica {
          Aritmetica aritmetica2 = new Aritmetica(5, 8);
          System.out.println("aritmetica2 = " + aritmetica2.a);
          System.out.println("aritmetica2 = " + aritmetica2);
+         //aritmetica1 = null; nunca utlizar esto, no se debe hacer
+         //System.gc(); método para limpiar residuos, es pesado, no utlizar
     } 
     
     public static void miMetodo() {
