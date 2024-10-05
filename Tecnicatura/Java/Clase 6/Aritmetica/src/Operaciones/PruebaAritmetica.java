a52165e91392bf434592f7185c448f3f9dde3aa2
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-10-05 20:28:42 -0300

    Clase 6 - Alcance de variables

--- Tecnicatura/Java/Clase 6/Aritmetica/src/Operaciones/PruebaAritmetica.java
+++ Tecnicatura/Java/Clase 6/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -3,6 +3,10 @@ package Operaciones;
 
 public class PruebaAritmetica {
     public static void main(String[] args) {
+        var a = 10; //Variables locales
+        int b = 7;
+        miMetodo ();
+        
         Aritmetica aritmetica1    = new Aritmetica();
         aritmetica1.a = 3;
         aritmetica1.b = 7;
@@ -22,5 +26,10 @@ public class PruebaAritmetica {
          System.out.println("aritmetica2 = " + aritmetica2);
     } 
     
-
+    public static void miMetodo() {
+        //a = 10; //Una variable esta limitada
+        System.out.println("Aqui hay otro método");
+        
+    }
+    
 } 
