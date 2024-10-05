03d6ecd04cb0fcbf4332b7e1a0a1ed00135911b3
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-10-05 20:11:07 -0300

    Clase 6 - Sobrecarga de métodos

---
+++ Tecnicatura/Java/Clase 6/Aritmetica/src/Operaciones/PruebaAritmetica.java
@@ -0,0 +1,26 @@
+
+package Operaciones;
+
+public class PruebaAritmetica {
+    public static void main(String[] args) {
+        Aritmetica aritmetica1    = new Aritmetica();
+        aritmetica1.a = 3;
+        aritmetica1.b = 7;
+        aritmetica1.sumarNumeros();
+        
+         int resultado = aritmetica1.sumarConRetorno();
+         System.out.println("resultado = " + resultado);
+         
+         resultado = aritmetica1.sumarConArgumentos(12, 26);
+         System.out.println("Resultado usando argumentos = "+resultado);
+         
+         System.out.println("aritmetica1 a: "+aritmetica1.a);
+         System.out.println("aritmetica1 b: "+aritmetica1.b);
+         
+         Aritmetica aritmetica2 = new Aritmetica(5, 8);
+         System.out.println("aritmetica2 = " + aritmetica2.a);
+         System.out.println("aritmetica2 = " + aritmetica2);
+    } 
+    
+
+} 
