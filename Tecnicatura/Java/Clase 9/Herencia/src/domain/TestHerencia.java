6d320682884acc02792f18503f32c350e84787c6
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-10-19 20:47:40 -0300

    Clase 9 Java - Tarea Herencia

--- Tecnicatura/Java/Clase 9/Herencia/src/domain/TestHerencia.java
+++ Tecnicatura/Java/Clase 9/Herencia/src/domain/TestHerencia.java
@@ -1,11 +1,15 @@
 
 package domain;
 
+import domain.Cliente;
 import domain.Empleado;
+import java.util.Date;
 
 public class TestHerencia {
     public static void maint(String[] args) {
         Empleado empleado1 = new Empleado("Ariel", 57000.0);
         System.out.println("empleado1 = " + empleado1);
+        
+        Cliente cliente1 = new Cliente(new Date(), true, "Bety", 'F', 32, "9 de Julio 1413");
     }
 }
