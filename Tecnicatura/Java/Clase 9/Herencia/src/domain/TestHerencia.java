a32479a9402caaad132278666c39a5f98cfa2f52
Sebastian Diaz <sebas.dbz19@gmail.com>
2024-10-19 17:00:37 -0300

    Clase 9 Java - Herencia Parte 2

---
+++ Tecnicatura/Java/Clase 9/Herencia/src/domain/TestHerencia.java
@@ -0,0 +1,11 @@
+
+package domain;
+
+import domain.Empleado;
+
+public class TestHerencia {
+    public static void maint(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
